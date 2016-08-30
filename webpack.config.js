6f3a4594651966b999db5028555662a6695dbdec
Sagi Medina <SagiMedina@Gmail.com>
2016-08-30 13:54:15 +0300

    gui and adib

--- webpack.config.js
+++ webpack.config.js
@@ -30,39 +30,3 @@ module.exports = {
     }
 };
 
-// module.exports = {
-//     entry: path.resolve(ROOT_PATH, 'src/js/index'),
-//     output: {
-//         path: path.join(ROOT_PATH, 'build'),
-//         filename: 'bundel.js'
-//     },
-//
-//     module: {
-//         loaders: [
-//             {
-//                 test: /\.css$/,
-//                 exclude: /node_modules/,
-//                 loaders: ['style', 'css'],
-//                 include: path.resolve(ROOT_PATH, 'src/css')
-//             },
-//             {
-//                 test: /\.(js|jsx|es6)$/,
-//                 exclude: /node_modules/,
-//                 loader: 'babel',
-//
-//
-//                 query: {
-//                     presets: [
-//                         'babel-preset-es2015',
-//                         'babel-preset-react',
-//                         'babel-preset-stage-0',
-//                     ].map(require.resolve),
-//
-//
-//                 }
-//
-//             }
-//         ]
-//     }
-// };
-

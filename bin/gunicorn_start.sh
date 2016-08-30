0b55a90b85bf8d63626739d3621442e1625162cf
gal_yaish <yaish2k@gmail.com>
2016-08-30 10:53:12 +0300

    update

---
+++ bin/gunicorn_start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+NAME="gvia_yadim_report"
+SERVERDIR=/home/tal/apis/gvia_yadim_report
+NUM_WORKERS=3
+USER=tal
+GROUP=www-data
+
+echo "Starting $NAME as `whoami`"
+echo "`pwd`"
+
+cd $SERVERDIR
+
+source ../.././.virtualenvs/gvia_yadim_report/bin/activate
+export PYTHONPATH=$SERVERDIR:$PYTHONPATH
+
+exec ../.././.virtualenvs/gvia_yadim_report/bin/gunicorn wsgi:app --name $NAME --workers $NUM_WORKERS --user=$USER -b 0.0.0.0:8008 --log-level=debug --log-file=- --timeout=600

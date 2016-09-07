1eda3ebc130f1eb4cf6aa44181eafb083ec9b5c6
gal yaish <galyaish@gals-mbp.esolutions.local>
2016-09-07 18:20:01 +0300

    error

--- flask_server.py
+++ flask_server.py
@@ -22,18 +22,14 @@ def multiple_routes(**kwargs):
 def get_xl_file():
     chosen_file = request.files['file']
     worker_name = request.form['worker']
-    try:
-        if chosen_file and allowed_file(chosen_file.filename):
-            filename = secure_filename(chosen_file.filename)
-            chosen_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            fn = FileManager()
-            fn.extract_all_comments_and_update_cem(os.path.join(app.config['UPLOAD_FOLDER'], filename), worker_name)
-            return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
-        else:
-            return json.dumps({'error': True}), 500, {'ContentType': 'application/json'}
-    except Exception as e:
-        print str(e)
-        return json.dumps({'error' : str(e)})
+    if chosen_file and allowed_file(chosen_file.filename):
+        filename = secure_filename(chosen_file.filename)
+        chosen_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        fn = FileManager()
+        fn.extract_all_comments_and_update_cem(os.path.join(app.config['UPLOAD_FOLDER'], filename), worker_name)
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    else:
+        return json.dumps({'error': True}), 500, {'ContentType': 'application/json'}
 
 
 def allowed_file(filename):
